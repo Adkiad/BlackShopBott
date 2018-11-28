eaa455ec8e9ccd6aa178ff5eff4ed8c14d88160c
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-11-28 15:06:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const botconfig = require("./botconfig.json");
-const Discord = require ("Discord.js");
+const Discord = require ("discord.js");
+const prefix = `$`
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 
@@ -42,4 +42,4 @@ bot.on(`ready`, ()=>{
 
 
 
-  bot.login(botconfig.token)
+bot.login(process.env.BOT_TOKEN)
