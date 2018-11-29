2b4f8eca1a0c6d45b2efcd156c8f68566294fe62
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-11-29 20:04:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const Discord = require ("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
+
+bot.on(`ready`, ()=>{
 bot.user.setStatus("dnd")
 bot.user.setGame(`$bc | Vampires`, "https://www.twitch.tv/azoqzmj")
+})
 bot.on('message', message => {
   if (message.content.startsWith("رابط")) {
     if(!message.author.bot)
