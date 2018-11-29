e6e91777a078bba26e8455aa6b1861c59c217983
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-11-29 20:00:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,21 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 bot.on('message', message => {
   if (message.content.startsWith("رابط")) {
-if(!message.member)
+    if(!message.author.bot)
 
 message.channel.createInvite({
       thing: true,
       maxUses: 100,
       maxAge: 86400
   }).then(invite =>
-    message.author.sendMessage(invite.url)
-  )
-message.channel.send("**تم ارسال الرابط برسالة خاصة**").then(()=>{
+    message.author.sendMessage(invite.url))
+
+message.channel.send("**تم ارسال الرابط برسالة خاصة**")
 message.author.send(`**مدة الرابط : يـوم
 عدد استخدامات الرابط : 100**`)
-
-
-  })
-}
+  }
 })
+  
 
 
 bot.login(process.env.BOT_TOKEN)
