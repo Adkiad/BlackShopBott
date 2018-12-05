0dea3c83c56e00952fc4a2160015fe346ee9beb9
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-12-05 15:48:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ bot.on(`message`, message=>{
   if (message.content.startsWith(prefix + "bc")) {
     if (message.author.id != "346066545107009537")
     if (message.author.id != "284151161291014144")
-    if (message.author.id != "284151161291014144") return;
+    if (message.author.id != "284151161291014144")
     if (!message.author.id != "346066545107009537") return;
     let args = message.content.split(" ").slice(1);
     var argresult = args.join(' '); 
