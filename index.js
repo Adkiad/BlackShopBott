32b6af1c31e93e4e7aebd42deb33843c409f4eee
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-12-05 15:25:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -43,19 +43,60 @@ const clean = text => {
     return text;
  }
 
-    bot.on("message", message => {
-      const args = message.content.split(" ").slice(1);
-      if (message.content.startsWith(prefix + "eval")) {
-        if (message.author.id != "284151161291014144") return;
-        try{
-          const code = args.join(" ");
-          let evaled = eval(code);
-          if (typeof evaled !== "string")
-          evaled = require("util").inspect(evaled);
-          message.channel.send(clean(evaled), {code:"xl"});
-        }catch (err){
-          message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+ bot.on("message", message => {
+  const args = message.content.split(" ").slice(1);
+  if (message.content.startsWith(prefix + "eval")) {
+    if (message.author.id != "284151161291014144") return;
+    try{
+      const code = args.join(" ");
+      let evaled = eval(code);
+      if (typeof evaled !== "string")
+      evaled = require("util").inspect(evaled);
+      message.channel.send(clean(evaled), {code:"xl"});
+    }catch (err){
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
 }
-      }
-    })
+  }
+})
+bot.on('message', message => {
+  if (message.content.split(' ')[0] == '%k')
+     message.guild.members.forEach( member => {
+       if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+
+         member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
+                                                    message.delete();
+          
+                                                  });
+          
+                                                });
+                                                bot.on("message", message => {
+
+  if (message.content.startsWith(prefix + "bc")) {
+    var args = message.content.substring(prefix.length).split(" ");
+    if (message.content.startsWith(prefix + "bc")) {
+              if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+              if (!args[1]) {
+                
+                     let embed3 = new Discord.RichEmbed()
+                         .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
+                           .setColor("#FF00FF")
+                              message.channel.sendEmbed(embed3);
+                
+                            } else {
+
+                
+                               let embed4 = new Discord.RichEmbed()
+                                                .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
+                                                    .setColor("#99999")
+                   
+                                                    message.channel.sendEmbed(embed4);
+                                          message.delete();
+
+                            }
+                          }
+                        }
+                      })
+                      
 bot.login(process.env.BOT_TOKEN)
