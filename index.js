04dcc30eac49269b17a61c0eadfe3e0317f8875a
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-12-19 17:45:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
 const Discord = require ("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
-const prefix = "$"
+const botconfig = require ("./botconfig.json")
 bot.commands = new Discord.Collection();
 
-bot.on(`message`, message=>{
+
+bot.on(`ready`, ()=>{
+  console.log(`${bot.user.username} is online!`);
+  console.log(`----------------`);
+  console.log(`VAMPIRES Bot- Script By : Azoqz`);
+  console.log(`----------------`);
+  console.log(`ON ${bot.guilds.size} Servers '     Script By : Azoqz ' `);
+  console.log(`----------------`);
+  console.log(`Logged in as ${bot.user.tag}!`);
+  bot.user.setStatus("dnd")
+  bot.user.setGame(`-bc | Kawaii Hangout`, "https://www.twitch.tv/azoqzmj")
+})  
+
+bot.on("message", async message => {
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+  
+  let prefix = `$`
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = message.content.split(" ").slice(1);
+
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+
+
   if (message.content.startsWith(prefix + "bc")) {
     if (message.author.id != "284151161291014144")
     if (message.author.id != "508002163457392660") return message.reply("ولدددد م عندك برمششششن")
-    var argsBC = message.content.split(' ').slice(1).join(' ');
+
+    var argsBC = message.content.split(" ").slice(1).join(' ');
     if(!argsBC) return err(message, "Type the message to send.");
-    message.guild.members.filter(m => !m.user.bot).forEach(m => {
-      m.send(argsBC.replace(/muser/g, m)).catch(err => console.log(err));
-      message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`)
+    message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+      m.send(argsBC.replace(/`muser`/g, m)).catch(err => console.log(err));
+
+      })
+      message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\`: عدد الاعضاء المستلمين`)
       message.delete();
-    })
-  }
+  }	
 })
-const clean = text => {
-  if (typeof(text) === "string")
-    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-  else
-    return text;
- }
-
- bot.on("message", message => {
-  const args = message.content.split(" ").slice(1);
-  if (message.content.startsWith(prefix + "eval")) {
-    if (message.author.id != "284151161291014144")
-     return;
-    try{
-      const code = args.join(" ");
-      let evaled = eval(code);
-      if (typeof evaled !== "string")
-      evaled = require("util").inspect(evaled);
-      message.channel.send(clean(evaled), {code:"xl"});
-    }catch (err){
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-}
-  }
-})
-
-
 bot.login(process.env.BOT_TOKEN)
