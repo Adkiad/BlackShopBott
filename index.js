6a10b2a23f25b97840d25a99aa72252dece481a4
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-12-19 17:13:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,55 +3,19 @@ const bot = new Discord.Client({disableEveryone: true});
 const prefix = "$"
 bot.commands = new Discord.Collection();
 
-bot.on(`ready`, ()=>{
-bot.user.setStatus("dnd")
-bot.user.setGame(`رابط | BlackShop`, "https://www.twitch.tv/azoqzmj")
-})
-bot.on('message', message => {
-  if (message.content.startsWith("رابط")) {
-    if(!message.author.bot)
-
-message.channel.createInvite({
-      thing: true,
-      maxUses: 100,
-      maxAge: 86400
-  }).then(invite =>
-    message.author.sendMessage(invite.url))
-
-message.channel.send("**تم ارسال الرابط برسالة خاصة**")
-message.author.send(`**مدة الرابط : يـوم
-عدد استخدامات الرابط : 100**`)
-  }
-  bot.on(`ready`, () =>{
-    console.log(`Bot Launched...`)
-
-  });
-
-  bot.on(`guildMemberAdd`, member => {
-    console.log(`User ` + member.user + `has joined the server!` )
-
-    var role = member.guild.roles.find(`name`, `Member`)
-
-    member.addRole(role)
-  });
-})
-
 bot.on(`message`, message=>{
   if (message.content.startsWith(prefix + "bc")) {
-    if (message.author.id != "346066545107009537")
     if (message.author.id != "284151161291014144")
-    if (message.author.id != "284151161291014144") return;
-    if (!message.author.id != "346066545107009537") return;
-    let args = message.content.split(" ").slice(1);
-    var argresult = args.join(' '); 
-    message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
-      m.send(`${argresult}\n ${m}`);
+    if (message.author.id != "508002163457392660") return message.reply("ولدددد م عندك برمششششن")
+    var argsBC = message.content.split(' ').slice(1).join(' ');
+    if(!argsBC) return err(message, "Type the message to send.");
+    message.guild.members.filter(m => !m.user.bot).forEach(m => {
+      m.send(argsBC.replace(/muser/g, m)).catch(err => console.log(err));
+      message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`)
+      message.delete();
     })
-    message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`)
-    message.delete();
   }
 })
-
 const clean = text => {
   if (typeof(text) === "string")
     return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
@@ -63,7 +27,6 @@ const clean = text => {
   const args = message.content.split(" ").slice(1);
   if (message.content.startsWith(prefix + "eval")) {
     if (message.author.id != "284151161291014144")
-    if (message.author.id != "346066545107009537")
      return;
     try{
       const code = args.join(" ");
